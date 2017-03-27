1536bc32fbcbab933424c2c117d5a7efe7830cf1
FT <ff755@outlook.com>
2017-03-27 18:01:21 +0800

    'demo'

---
+++ src/components/Hello.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+     <editor id="editor_id" height="500px" width="700px" :content="editorText"
+            pluginsPath="/static/kindeditor/plugins/"
+            :loadStyleMode="false"
+            @on-content-change="onContentChange"></editor>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+       editorText: ''
+    }
+  }, 
+  methods: {
+    onContentChange (val) {
+      this.editorText = val
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+// 引入 vue-kikindeditor 需要的文件
+import VueKindEditor from 'vue-kindeditor'
+import 'kindeditor/kindeditor-all-min.js'
+import 'kindeditor/themes/default/default.css'
+
+Vue.config.productionTip = false
+
+// 注册 vue-kikindeditor
+Vue.use(VueKindEditor)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
